9cadc78739f6ca6963d4df2d5436fea33b60217c
LSChyi <alan81920@gmail.com>
2020-05-12 16:15:30 +0800

    refactor main

--- main.go
+++ main.go
@@ -20,6 +20,36 @@ func main() {
 	filePath := flag.String("o", filepath.Join(currentDir, "output.csv"), "output file name")
 	flag.Parse()
 
+	printWelcomeMessage()
+
+	defer log.Info("Thank you for using frequency logger, bye-bye!")
+
+	log.Infof("The output is stored to file %s", *filePath)
+	frequencyLogger, err := newLogger(*filePath)
+	if err != nil {
+		log.WithError(err).Fatal("Can not create frequency logger")
+	}
+	defer frequencyLogger.close()
+	go frequencyLogger.run()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	select {
+	case <-sig:
+	case <-frequencyLogger.doneCh:
+	}
+	fmt.Println()
+}
+
+func printWelcomeMessage() {
+	printLogo()
+	fmt.Println("ErgoKB key frequency logger - log your key press to csv for further analysis")
+	fmt.Println("for more information, please visit https://www.ergokb.tw")
+	fmt.Println("To stop, use Ctrl-c to leave program")
+	fmt.Printf("\n\n")
+}
+
+func printLogo() {
 	asciiArt :=
 		`
 
@@ -54,25 +84,4 @@ func main() {
                            *&&&&&
 	`
 	fmt.Println(asciiArt)
-	fmt.Println("ErgoKB key frequency logger - log your key press to csv for further analysis")
-	fmt.Println("for more information, please visit https://www.ergokb.tw")
-	fmt.Println("To stop, use Ctrl-c to leave program")
-	fmt.Printf("\n\n")
-	defer log.Info("Thank you for using frequency logger, bye-bye!")
-
-	log.Infof("The output is stored to file %s", *filePath)
-	frequencyLogger, err := newLogger(*filePath)
-	if err != nil {
-		log.WithError(err).Fatal("Can not create frequency logger")
-	}
-	defer frequencyLogger.close()
-	go frequencyLogger.run()
-
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	select {
-	case <-sig:
-	case <-frequencyLogger.doneCh:
-	}
-	fmt.Println()
 }
